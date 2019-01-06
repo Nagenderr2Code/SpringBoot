5a91e1f360114e2920076fa8400afb49ec5a3137
Nagenderr2Code <nagender2u@yahoo.co.in>
2019-01-06 11:07:30 -0500

    Boot Initial Commit

---
+++ src/main/java/com/springboot/proj/bootframe/BootframeApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.proj.bootframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootframeApplication.class, args);
+	}
+
+}
+
